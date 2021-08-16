ff16af36b39836fc9b1bb53afb3968f7372c4888
Andre Boussarath <andreboussarath@gmail.com>
2021-08-16 18:35:20 -0500

    Static db credentials added

--- server/db.js
+++ server/db.js
@@ -1,6 +1,13 @@
 const { Client } = require('pg');
 
-const client = new Client();
+const client = new Client({
+  user: 'ubuntu',
+  host: 'ec2-54-164-75-75.compute-1.amazonaws.com',
+  database: 'sdc',
+  password: 'ubuntu',
+  port: 5432,
+})
+
 client.connect();
 
 function close() {
@@ -8,3 +15,4 @@ function close() {
 }
 
 module.exports = { client, close }
+
