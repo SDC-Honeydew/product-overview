799f24a9045912aaba076769273b934ea1dcc297
Andre Boussarath <andreboussarath@gmail.com>
2021-08-16 20:12:43 -0500

    Fixed db credentials

--- server/db.js
+++ server/db.js
@@ -1,12 +1,6 @@
 const { Client } = require('pg');
 
-const client = new Client({
-  user: 'ubuntu',
-  host: 'ec2-54-164-75-75.compute-1.amazonaws.com',
-  database: 'sdc',
-  password: 'ubuntu',
-  port: 5432,
-})
+const client = new Client()
 
 client.connect();
 
